e7d5144dbf979774a36e5474146dada06000bac8
YangD17 <91928127+YangD17@users.noreply.github.com>
2023-12-02 22:07:02 -0500

    Create MainWindowTest.java

---
+++ src/test/MainWindowTest.java
@@ -0,0 +1,45 @@
+package test;
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainWindowTest {
+
+    @Test
+    void convert() {
+        ArrayList<Currency> currencies = new ArrayList<>();
+        Currency currency1 = new Currency("US Dollar", "USD");
+        Currency currency2 = new Currency("Euro", "EUR");
+        currencies.add(currency1);
+        currencies.add(currency2);
+
+        currency1.getExchangeValues().put("EUR", 0.85);
+
+        Double result = MainWindow.convert("US Dollar", "Euro", currencies, 100.0);
+
+        assertEquals(85.0, result, 0.01);
+    }
+    @Test
+    void testConvertNoCurrency1() {
+        ArrayList<Currency> currencies = new ArrayList<>();
+        Currency currency2 = new Currency("Euro", "EUR");
+        currencies.add(currency2);
+
+        Double result = MainWindow.convert("USD", "EUR", currencies, 100.0);
+
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    void testConvertNoCurrency2() {
+        ArrayList<Currency> currencies = new ArrayList<>();
+        Currency currency1 = new Currency("US Dollar", "USD");
+        currencies.add(currency1);
+
+        Double result = MainWindow.convert("USD", "EUR", currencies, 100.0);
+
+        assertEquals(0.0, result);
+    }
+}
